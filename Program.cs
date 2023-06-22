a7156b46ca1447491528dee75b89dc4d0900af61
Curllly <pavelllageev@gmail.com>
2023-06-23 03:02:13 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Validation;
+
+Student student = new Student()
+{
+    Age = 55,
+    Course = 5,
+    Name = "Игорь",
+    Surname = "Павлов",
+};
+
+var validator = new StudentValidation();
+var validationResult = validator.Validate(student);
+if (!validationResult.IsValid)
+{
+    foreach (var error in validationResult.Errors)
+    {
+        Console.WriteLine(error.ErrorMessage);
+    }
+}
---
+++ StudentValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    public class StudentValidation : AbstractValidator<Student>
+    {
+        public StudentValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Имя не должно быть пустым!");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Фамилия не должна быть пустой!");
+            RuleFor(x => x.Age).LessThan(100).WithMessage("Возраст должен быть меньше 100");
+            RuleFor(x => x.Age).GreaterThan(0).WithMessage("Возраст должен быть больше 0");
+            RuleFor(x => x.Course).
+                NotEmpty().
+                LessThan(5).WithMessage("Курс должен быть меньше 5").
+                GreaterThan(0).WithMessage("Курс должен быть больше 0");
+        }
+    }
+}
